b9641ee68dbb17ee8c32c3f768ce8778ab7c2bc6
Alyssandro Ramos <dyogo.alyssandro@gmail.com>
2023-10-18 09:44:12 -0300

    Update testCreator.java

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/testCreator.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/testCreator.java
@@ -8,7 +8,7 @@ public interface testCreator {
     ArrayList<Question> getQuestionsRandom(int quantEasyQuest, int quantyModerQuest, int quantHardQuest, ArrayList<Question> questions);
     
     /* Aqui ele apenas irá reordenar as questões escolhidas pelo professor */
-    ArrayList<Question> getQuestionsRandom(ArrayList<Question> questions);
+    ArrayList<Question> getQuestionsRandom(ArrayList<Question> questionsFromDB);
     /*Elas não retorna nada pq essa função já vai gerar e 
     baixar na máquina do professor a prova criada*/
 
