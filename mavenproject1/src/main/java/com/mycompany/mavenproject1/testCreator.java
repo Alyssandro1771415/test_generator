d60ee8c5ba59d746f4d44b955a18bbc00445a81e
Alyssandro Ramos <dyogo.alyssandro@gmail.com>
2023-10-18 09:42:39 -0300

    18/10
    Criação da interface para a classe geradora da prova cogitando o caso das questões serem sorteadas ou escolhidas pelo usuário

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/testCreator.java
@@ -0,0 +1,17 @@
+package com.mycompany.mavenproject1;
+
+import java.util.ArrayList;
+
+public interface testCreator {
+
+    /* Aqui será para gerar aleatoriamente */
+    ArrayList<Question> getQuestionsRandom(int quantEasyQuest, int quantyModerQuest, int quantHardQuest, ArrayList<Question> questions);
+    
+    /* Aqui ele apenas irá reordenar as questões escolhidas pelo professor */
+    ArrayList<Question> getQuestionsRandom(ArrayList<Question> questions);
+    /*Elas não retorna nada pq essa função já vai gerar e 
+    baixar na máquina do professor a prova criada*/
+
+    void generateTest(ArrayList<Question> questions);
+
+}
