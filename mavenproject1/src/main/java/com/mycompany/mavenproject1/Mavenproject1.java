11e9cc9d6270a9eb004cfca22faa97aa017cc42f
Alyssandro Ramos <dyogo.alyssandro@gmail.com>
2023-10-19 15:52:05 -0300

    19/10

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -7,7 +7,7 @@ public class Mavenproject1 {
 
     public static void main(String[] args) throws SQLException {
         
-        queryExecutions consultor = new queryExecutions();
+        /*queryExecutions consultor = new queryExecutions();
 
         consultor.dataUpload("Português", "Tempos Verbais", "Qual o tempo verbal da frase: 'Eu vejo o futuro repetir o passado'", 1, "Passado", "Futuro", "Presente");
 
@@ -17,7 +17,16 @@ public class Mavenproject1 {
             System.out.println(i.getQuestion());
         }
 
-        consultor.dataDelete(11);
+        consultor.dataDelete(11);*/
+
+        ArrayList<Question> ListaQuestoes = new ArrayList<>();
+        ListaQuestoes.add(new Question(1, "Matemática", "Derivadas","Resolva a equação f'(x) = 2x + 1 = 0.", 1));
+        ListaQuestoes.add(new Question(4, "Matemática", "Soma de Frações" ,"Resolva a equação 2/5 + 7/8.", 2));
+        ListaQuestoes.add(new Question(7, "Matemática", "Derivadas" ,"f'(x) = x^2 + 7x + 5.", 3));
+        ListaQuestoes.add(new Question(9, "Matemática", "Operações com conjuntos","Resolva a equação VUW.", 2));
+
+        Test geradorTest = new Test();
+        geradorTest.gerarArquivo(ListaQuestoes);
 
     }
 }
---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Test.java
@@ -0,0 +1,84 @@
+package com.mycompany.mavenproject1;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
+
+public class Test {
+
+    public void gerarArquivo(ArrayList<Question> listQuestions) {
+        FileOutputStream out = null;
+        XWPFDocument document = new XWPFDocument();
+
+        try {
+            out = new FileOutputStream(new File("Prova.docx"));
+
+            XWPFParagraph paragrafo = document.createParagraph();
+            XWPFRun runPaRun1 = paragrafo.createRun();
+
+            runPaRun1.addBreak();
+            runPaRun1.setText("_________________________________________________________________________________", 0);
+            runPaRun1.addBreak();
+            runPaRun1.addBreak();
+            runPaRun1.addTab();
+            runPaRun1.setText(
+                    "Instituição: .....................................................................................................",
+                    1);
+            runPaRun1.addBreak();
+            runPaRun1.addTab();
+            runPaRun1.setText(
+                    "Aluno: ............................................................................................................",
+                    2);
+            runPaRun1.addBreak();
+            runPaRun1.addTab();
+            runPaRun1.setText("Matrícula: ............................. \t\t\t\t\t Nota: .............", 3);
+            runPaRun1.addBreak();
+            runPaRun1.removeTab();
+            runPaRun1.setText("_________________________________________________________________________________", 4);
+            runPaRun1.addBreak();
+            runPaRun1.addBreak();
+            runPaRun1.setBold(true);
+
+            XWPFParagraph paragrafo2 = document.createParagraph();
+            XWPFRun runPaRun2 = paragrafo2.createRun();
+
+            for (int i = 0; i <= listQuestions.size() - 1; i++) {
+
+                runPaRun2.addTab();
+                runPaRun2.setText(Integer.toString(i + 1) + "°) ");
+                runPaRun2.setText(listQuestions.get(i).getQuestion());
+                runPaRun2.addBreak();
+                runPaRun2.addBreak();
+                runPaRun2.addBreak();
+                runPaRun2.addBreak();
+                runPaRun2.addBreak();
+                runPaRun2.addBreak();
+
+            }
+
+            document.write(out);
+            document.close();
+
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Test.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Test.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                if (out != null) {
+                    out.close();
+                }
+            } catch (IOException ex) {
+                Logger.getLogger(Test.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+}
